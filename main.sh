72b6420235d67d406c892647ffb29a7a989ec43f
Nathan Papapietro <npapapietro95@gmail.com>
2022-12-08 14:57:04 -0600

    Formatted messages

--- main.sh
+++ main.sh
@@ -11,33 +11,46 @@ TOKEN=$(cat ${SERVICE_ACCOUNT}/token)
 CACERT=${SERVICE_ACCOUNT}/ca.crt
 
 function getDetails(){
-    local result=$( curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET "${API_SERVER}/api/v1/namespaces/${K8S_NAMESPACE}/pods/${K8S_POD_NAME}" )
+    local result=$( curl -sf --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET "${API_SERVER}/api/v1/namespaces/${K8S_NAMESPACE}/pods/${K8S_POD_NAME}" )
     echo $result
 }
 
 # Returns the number of istio-proxy pods running
 # Expected 1 or 0
 function getProxyRunningCount(){
-    echo `getDetails` | jq  --arg proxy "$ISTIO_PROXY_NAME" '[.status.containerStatuses[] | select(.name==$proxy and .state.running)] | length'
+    local response=`getDetails`
+    if [[ "$response" != "" ]];
+    then
+        echo $response | jq  --arg proxy "$ISTIO_PROXY_NAME" '[.status.containerStatuses[] | select(.name==$proxy and .state.running)] | length';
+    fi
 }
 
 function getPodRunningCount(){
-    echo `getDetails` | jq  --arg proxy "$ISTIO_PROXY_NAME" --arg self "$K8S_SELF_NAME" '[.status.containerStatuses[] | select(.name!=$proxy and .name!=$self) | (.state.terminated.exitCode // 1) ] | add'
+    local response=`getDetails`
+    if [[ "$response" != "" ]];
+    then
+        echo $response  | jq  --arg proxy "$ISTIO_PROXY_NAME" --arg self "$K8S_SELF_NAME" '[.status.containerStatuses[] | select(.name!=$proxy and .name!=$self) | (.state.terminated.exitCode // 1) ] | add';
+    fi
 }
 
 while true;
 do
-    echo "Checking for completion"
-    podRunning=`getPodRunningCount`
-    proxyRunning=`getProxyRunningCount`
-    if [[ "$podRunning" -eq "0" && "$proxyRunning" -eq "1" ]]; then
-        curl -sf -XPOST http://$ISTIO_ENDPOINT/quitquitquit
-        exit 0
+    sleep $SLEEP_INTERVAL
+    PODRUNNING=`getPodRunningCount`
+    PROXYRUNNING=`getProxyRunningCount`
+    echo "{\"message\": \"Checking for completion.\"}"
+    if [[ "$PODRUNNING" == "" &&  "$PROXYRUNNING" == "" ]]; then
+        echo "{\"message\": \"Pod not ready yet.\"}"
+    else
+        echo "{\"message\": \"Found ${PODRUNNING} out of 0 expected job containers running.\"}"
+        echo "{\"message\": \"Found ${PROXYRUNNING} out of 1 istio proxy containers running.\"}"
     fi
     
-    echo "Found $podRunning out of 0 expected job containers running."
-    echo "Found $proxyRunning out of 1 istio proxy containers running."
-    echo "Sleeping for $SLEEP_INTERVAL"
+    if [[ "$PODRUNNING" -eq "0" && "$PROXYRUNNING" -eq "1" ]]; then
+        echo "{\"message\": \"Quitting conditions met, calling /quitquitquit on istio-proxy\"}"
+        curl -sf -XPOST http://$ISTIO_ENDPOINT/quitquitquit &>/dev/null
+        exit 0
+    fi
+    echo "{\"message\": \"Sleeping for ${SLEEP_INTERVAL}.\"}"
     
-    sleep $SLEEP_INTERVAL
 done
